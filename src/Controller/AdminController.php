3cbaeb35472f14909c5b6096a8eee39864d17d96
kluknulo-star <work.klkv@mail.ru>
2022-09-24 20:52:50 +0300

    Prototype #1

---
+++ src/Controller/AdminController.php
@@ -0,0 +1,116 @@
+<?php
+
+namespace App\Controller;
+
+use App\Entity\BookingRequest;
+use App\Form\BookingType;
+use App\Repository\BookingRequestRepository;
+use Doctrine\ORM\EntityManagerInterface;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+
+class AdminController extends AbstractController
+{
+    /**
+     * @Route("/admin", name="homepage_admin")
+     */
+    public function index(BookingRequestRepository $calendar): Response
+    {
+        $events = $calendar->findAll();
+//        dd($events);
+
+        $rdvs=[];
+        foreach($events as $event){
+            $rdvs[] = [
+                'id' => $event->getId(),
+                'start' => $event->getDateStart()->format('Y-m-d H:i:s'),
+                'end' => $event->getDateEnd()->format('Y-m-d H:i:s'),
+                'title' => $event->getTitle(),
+                'color'=> sprintf('#%02X%02X%02X', rand(0, 255), rand(0, 255), rand(0, 255)),
+            ];
+        }
+
+        $data = json_encode($rdvs);
+        return $this->render('admin/index.html.twig', compact('data'));
+    }
+
+    /**
+     * @Route("/new", name="booking_new")
+     */
+    public function new(Request $request, EntityManagerInterface $entityManager): Response
+    {
+        $booking = new BookingRequest();
+        $form = $this->createForm(BookingType::class, $booking);
+        $form->handleRequest($request);
+
+        if ($form->isSubmitted() && $form->isValid()) {
+            $entityManager->persist($booking);
+            $entityManager->flush();
+
+            return $this->redirectToRoute('homepage', [], Response::HTTP_SEE_OTHER);
+        }
+
+        return $this->renderForm('admin/new.html.twig', [
+            'booking' => $booking,
+            'form' => $form,
+        ]);
+    }
+
+    /**
+     * @Route("/{id}", name="booking_show", methods={"GET"})
+     */
+    public function show(BookingRequest $booking): Response
+    {
+        return $this->render('admin/show.html.twig', [
+            'booking' => $booking,
+        ]);
+    }
+
+
+    /**
+     * @Route("/{id}/edit", name="booking_edit", methods={"GET"})
+     */
+    public function edit(Request $request, BookingRequest $booking, EntityManagerInterface $entityManager): Response
+    {
+        $form = $this->createForm(BookingType::class, $booking);
+        $booking->setDateUpdate( new \DateTimeImmutable());
+        $form->handleRequest($request);
+
+        if ($form->isSubmitted() && $form->isValid()) {
+            $entityManager->flush();
+
+            return $this->redirectToRoute('booking_index_admin', [], Response::HTTP_SEE_OTHER);
+        }
+
+        return $this->renderForm('admin/edit.html.twig', [
+            'booking' => $booking,
+            'form' => $form,
+        ]);
+    }
+
+    /**
+     * @Route("/list_admin", name="booking_index_admin", methods={"GET"})
+     */
+    public function list(BookingRequestRepository $bookingRepository): Response
+    {
+        return $this->render('admin/booking_list.html.twig', [
+            'bookings' => $bookingRepository->findAll(),
+        ]);
+    }
+
+
+    /**
+     * @Route("/{id}/delete", name="booking_delete", methods={"GET"})
+     */
+    public function delete(Request $request, BookingRequest $booking, EntityManagerInterface $entityManager): Response
+    {
+        if ($this->isCsrfTokenValid('delete'.$booking->getId(), $request->request->get('_token'))) {
+            $entityManager->remove($booking);
+            $entityManager->flush();
+        }
+
+        return $this->redirectToRoute('homepage_admin', [], Response::HTTP_SEE_OTHER);
+    }
+}
---
+++ src/Entity/Room.php
@@ -0,0 +1,143 @@
+<?php
+
+namespace App\Entity;
+
+use App\Repository\RoomRepository;
+use Doctrine\ORM\Mapping as ORM;
+
+/**
+ * @ORM\Entity(repositoryClass=RoomRepository::class)
+ */
+class Room
+{
+    /**
+     * @ORM\Id
+     * @ORM\GeneratedValue
+     * @ORM\Column(type="integer")
+     */
+    private $id;
+
+    /**
+     * @ORM\Column(type="string", length=25)
+     */
+    private $type;
+
+    /**
+     * @ORM\Column(type="string", length=255, nullable=true)
+     */
+    private $description;
+
+    /**
+     * @ORM\Column(type="boolean")
+     */
+    private $active;
+
+    /**
+     * @ORM\Column(type="datetime")
+     */
+    private $date_create;
+
+    /**
+     * @ORM\Column(type="datetime", nullable=true)
+     */
+    private $date_update;
+
+    /**
+     * @ORM\Column(type="integer")
+     */
+    private $id_user_create;
+
+    /**
+     * @ORM\Column(type="integer", nullable=true)
+     */
+    private $id_user_update;
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    public function getType(): ?string
+    {
+        return $this->type;
+    }
+
+    public function setType(string $type): self
+    {
+        $this->type = $type;
+
+        return $this;
+    }
+
+    public function getDescription(): ?string
+    {
+        return $this->description;
+    }
+
+    public function setDescription(?string $description): self
+    {
+        $this->description = $description;
+
+        return $this;
+    }
+
+    public function getActive(): ?bool
+    {
+        return $this->active;
+    }
+
+    public function setActive(bool $active): self
+    {
+        $this->active = $active;
+
+        return $this;
+    }
+
+    public function getDateCreate(): ?\DateTimeInterface
+    {
+        return $this->date_create;
+    }
+
+    public function setDateCreate(\DateTimeInterface $date_create): self
+    {
+        $this->date_create = $date_create;
+
+        return $this;
+    }
+
+    public function getDateUpdate(): ?\DateTimeInterface
+    {
+        return $this->date_update;
+    }
+
+    public function setDateUpdate(?\DateTimeInterface $date_update): self
+    {
+        $this->date_update = $date_update;
+
+        return $this;
+    }
+
+    public function getIdUserCreate(): ?int
+    {
+        return $this->id_user_create;
+    }
+
+    public function setIdUserCreate(int $id_user_create): self
+    {
+        $this->id_user_create = $id_user_create;
+
+        return $this;
+    }
+
+    public function getIdUserUpdate(): ?int
+    {
+        return $this->id_user_update;
+    }
+
+    public function setIdUserUpdate(?int $id_user_update): self
+    {
+        $this->id_user_update = $id_user_update;
+
+        return $this;
+    }
+}
